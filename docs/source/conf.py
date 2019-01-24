6bc6a265dcf63d22d63dcf8e57fb3f64f3dcc1ee
Adam Naylor <adamnaylor@gmail.com>
2019-01-24 16:43:48 +0000

    Changed theme to rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,12 +19,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ecka-doc'
-copyright = '2019, Adam Naylor'
+project = 'Ecka'
+copyright = '2019, Imbick'
 author = 'Adam Naylor'
 
 # The short X.Y version
-version = ''
+version = '0.1.1'
 # The full version, including alpha/beta/rc tags
 release = '0.1.1'
 
@@ -75,7 +75,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to ecka-doc's documentation!
+Ecka v0.1.1 documentation
 ====================================
 
 .. toctree::
